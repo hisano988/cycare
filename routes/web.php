e1719381e7b2dc8a0eeec64c7b2cc9d9e6dce10f
hisano <m.hisano@gmail.com>
2024-11-20 22:40:19 +0900

    [fix]ルートのデグレ修正

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WebController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +13,12 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::prefix('/home')->group(function () {
+        Route::get('/', [WebController::class, 'showHome'])->name('web.home');
+
+        Route::post('/record', [WebController::class, 'record'])->name('web.home.record');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
