04ecf5b49459df4344f6a10cab02444cccedcf0f
hisano <m.hisano@gmail.com>
2024-11-21 13:36:54 +0900

    [update]レイアウト共通化

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,41 +1,43 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@php
+use Illuminate\Support\Carbon;
+@endphp
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('ホーム') }}
+        </h2>
+    </x-slot>
 
-        <title>Home</title>
-    </head>
-    <body>
-        <div>ホーム画面</div>
-        <div>
-            @php
-                use Illuminate\Support\Carbon;
-            @endphp
-            <div>
-                @if ($cycle->isPredictable())
-                    @php
-                        $nextDay = $cycle->predictNextStartDate();
-                        $remainingDays = Carbon::today()->diffInDays($nextDay);
-                    @endphp
-                    @if($remainingDays >= 0)
-                        あと{{ $remainingDays }}日
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                <div>
+                    @if ($cycle->isPredictable())
+                        @php
+                            $nextDay = $cycle->predictNextStartDate();
+                            $remainingDays = Carbon::today()->diffInDays($nextDay);
+                        @endphp
+                        @if($remainingDays >= 0)
+                            あと{{ $remainingDays }}日
+                        @else
+                            {{ -1 * $remainingDays }}日超過
+                        @endif
                     @else
-                        {{ -1 * $remainingDays }}日超過
+                        -
                     @endif
-                @else
-                    -
-                @endif
+                </div>
+                <div>
+                    <form action="{{ route('web.home.record') }}" method="post">
+                        @csrf
+                        <input type="text" name="start_date" value={{ \Carbon\Carbon::now()->format('Y-m-d') }} />
+                        <input type="checkbox" name="is_calc_target" checked />
+                        <button type="submit">記録する</button>
+                    </form>
+                </div>
+                <button>設定</button>
+                    </div>
             </div>
-            <div>
-                <form action="{{ route('web.home.record') }}" method="post">
-                    @csrf
-                    <input type="text" name="start_date" value={{ \Carbon\Carbon::now()->format('Y-m-d') }} />
-                    <input type="checkbox" name="is_calc_target" checked />
-                    <button type="submit">記録する</button>
-                </form>
-            </div>
-            <button>設定</button>
         </div>
-     </body>
-</html>
+    </div>
+</x-app-layout>
