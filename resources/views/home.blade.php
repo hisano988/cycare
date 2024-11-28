d2ddde95bcbb6571d978d55d09a47de195cdc12d
hisano <m.hisano@gmail.com>
2024-11-28 22:50:43 +0900

    [update]記録モーダルの表示位置調整

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,6 +20,32 @@ $endDate = $endOfMonth->copy()->addDays($addCntToSaturday);
         </h2>
     </x-slot>
 
+    <div class="modal" tabindex="-1" id="recordModal">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <form action="{{ route('web.home.record') }}" method="post">
+                <div class="modal-header">
+                  <h5 class="modal-title">周期を記録する</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                @csrf
+                <input class="form-control" type="date" name="start_date" value={{ \Carbon\Carbon::now()->format('Y-m-d') }} />
+                <label class="form-check-label" for="isCalcTarget">
+                    周期予測の計算対象に含める
+                </label>
+                <input class="form-check-input" id="isCalcTarget" type="checkbox" name="is_calc_target" checked />
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">閉じる</button>
+                    <button type="submit" class="btn btn-primary">保存</button>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    <div>
+
     <div class="py-12">
         <div class="d-flex justify-content-center">
             @if ($cycle->isPredictable())
@@ -76,37 +102,17 @@ $endDate = $endOfMonth->copy()->addDays($addCntToSaturday);
                 記録する
             </button>
         </div>
-
-        <div class="modal" tabindex="-1" id="recordModal">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <form action="{{ route('web.home.record') }}" method="post">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Modal title</h5>
-                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                    @csrf
-                    <input class="form-control" type="date" name="start_date" value={{ \Carbon\Carbon::now()->format('Y-m-d') }} />
-                    <label class="form-check-label" for="isCalcTarget">
-                        周期予測の計算対象に含める
-                    </label>
-                    <input class="form-check-input" id="isCalcTarget" type="checkbox" name="is_calc_target" checked />
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">閉じる</button>
-                        <button type="submit" class="btn btn-primary">保存</button>
-                    </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        <div>
         <button>設定</button>
     </div>
 </x-app-layout>
 <style>
     /* TODO: 移動 */
+    .modal-dialog{
+        display: flex;
+        align-items: center;
+        min-height: 100%;
+    }
+
     .col-today {
         color:green !important;
     }
